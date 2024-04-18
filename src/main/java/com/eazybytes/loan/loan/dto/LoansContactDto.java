be65dc13ae613c48edabadbd4fd741caf9982f98
Rajnish Shonkhia <rshonkhia65@gmail.com>
2024-04-18 21:07:05 +0530

    first commit

---
+++ src/main/java/com/eazybytes/loan/loan/dto/LoansContactDto.java
@@ -0,0 +1,11 @@
+package com.eazybytes.loan.loan.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "loans")
+public record LoansContactDto(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+
+}
